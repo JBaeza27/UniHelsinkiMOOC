c83f6adaef5820b6fe53514d4f35c73fb6ca6725
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-26 14:59:36 -0500

    completed part 4. challenge 17

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -9,6 +9,21 @@ public class Items {
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true){
+            System.out.println("Name: ");
+            String input = scanner.nextLine();
+            
+            if(input.isEmpty()){
+                break;
+            }
+            
+            items.add(new Item(input));
+        }
+        for(Item item : items){ //for every item in the in items print out the item. from the arrayList items.
+            System.out.println(item);
+        }
+        
 
     }
 }
