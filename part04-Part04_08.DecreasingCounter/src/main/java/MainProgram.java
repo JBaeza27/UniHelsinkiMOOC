df8540186ff5da810916e51dc3328ae4b034dacd
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-25 19:56:08 -0500

    completed part 4. challenge 8

--- part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
+++ part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
@@ -2,10 +2,13 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(10);
+        DecreasingCounter counter = new DecreasingCounter(20);
 
         counter.printValue();
         counter.decrement();
         counter.decrement();
+        counter.printValue();
+        counter.reset();
+        counter.printValue();
     }
 }
