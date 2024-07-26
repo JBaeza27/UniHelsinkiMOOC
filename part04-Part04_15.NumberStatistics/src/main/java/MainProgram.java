fab9ea96feb65f10915c85bcd32b3ee77041aabb
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-26 00:09:18 -0500

    completed part 4. challenge 11,12,13,14,15.

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,29 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        
+        Statistics sum = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+        
+        System.out.println("Enter numbers: ");
+        
+        while(true){
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input == -1){
+                System.out.println("Sum: " + sum.sum());
+                System.out.println("Sum of even numbers: " + even.sum());
+                System.out.println("Sum of odd numbers:  " + odd.sum());
+                break;
+            }else{
+                sum.addNumber(input);
+            }
+            if(input % 2 == 0){
+                even.addNumber(input);
+            }
+            else{
+                odd.addNumber(input);
+            }
+        }
     }
 }
