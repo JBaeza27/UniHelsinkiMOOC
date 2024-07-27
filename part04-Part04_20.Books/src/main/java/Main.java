d4620393d84bf558fa05a0dcdcbd0b0e216af1b2
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-27 15:02:08 -0500

    completed part 4. challenge 20

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,37 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> collection = new ArrayList<>();
+        
+        while(true){
+            System.out.println("Title: ");
+            String title = scanner.nextLine();
+            
+            if(title.isEmpty()){
+                break;
+            }
+            
+            System.out.println("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            
+            System.out.println("Publication year: ");
+            int publicationYear = Integer.valueOf(scanner.nextLine());
+            
+            collection.add(new Book(title, pages, publicationYear));
+        }
+        
+        
+        System.out.println("What information will be printed? ");
+        String input = scanner.nextLine();
+       
+       for(Book book : collection){
+           if(input.equals("everything")){
+               System.out.println(book);
+           }else if(input.equals("name")){
+               System.out.println(book.getTitle());
+           }
+       } 
+        
     }
 }
