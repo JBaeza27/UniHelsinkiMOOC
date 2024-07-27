f71e15a857bd00ac9467945ff5746ae08177303e
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-27 13:52:27 -0500

    completed part 4. challenge 19

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,34 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            
+            if (name.isEmpty()){
+                break;
+            }
+            
+            System.out.println("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            
+           
+            programs.add(new TelevisionProgram(name, duration));
+            
+            
+            
+            
+        }
+        System.out.println("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        
+        for(TelevisionProgram tv : programs){
+            if(tv.getDuration() <= maxDuration){
+                System.out.println(tv);
+            }
+        }
 
     }
 }
