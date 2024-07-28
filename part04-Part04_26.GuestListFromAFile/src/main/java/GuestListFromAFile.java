ab4944be59d57fb3b500087672175dc088201f86
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-28 14:55:24 -0500

    completed part 4. challenge 26

--- part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -13,6 +13,15 @@ public class GuestListFromAFile {
 
         ArrayList<String> list = new ArrayList<>();
         // implement reading the file here.
+        
+        try(Scanner readInput = new Scanner(Paths.get(file))){
+            while(readInput.hasNextLine()){
+                list.add(readInput.nextLine());
+            }
+        }catch (Exception e){
+            System.out.println("Error" + e.getMessage());
+        }
+        
         System.out.println("");
 
         System.out.println("Enter names, an empty line quits.");
