8cefe84ef1c8d04cde8f8225f1691e1ae0e39a48
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-28 02:04:03 -0500

    completed part 4. challenge 21

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -1,10 +1,22 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> list = new ArrayList<>();
+        
+        while(true){
+            String input = scanner.nextLine();
+            
+            if(input.contains("end")){
+                break;
+            }
+            list.add(input);
+            
+        }
+        System.out.println(list.size());
     }
 }
