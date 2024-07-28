40bf8668f6cfecdc4574eeb5de6a0076f6a45e5b
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-28 14:32:09 -0500

    completed part 4. challenge 25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,18 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Which file should have its contents printed? ");
+        String input = scanner.nextLine();
+        
+        try(Scanner fileReader = new Scanner(Paths.get(input))){
+            while (fileReader.hasNextLine()){
+                
+                
+                System.out.println(fileReader.nextLine());
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
