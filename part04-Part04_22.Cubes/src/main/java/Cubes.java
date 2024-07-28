b7ea1b374f645af95874afb83c5cd82613ad7898
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-28 02:27:27 -0500

    completed part 4. challenge 22

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,19 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true){
+            String input = scanner.nextLine();
+            
+            if(input.contains("end")){
+                break;
+            }
+      
+            int number = Integer.valueOf(input); /*converts input to valueOf number which is int.
+                                                   we do this to consider one total 'input'.
+                                                                             */
+                                                     
+            System.out.println(number * number * number);
+        }
     }
 }
