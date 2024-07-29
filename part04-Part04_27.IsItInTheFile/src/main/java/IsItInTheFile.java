9b8d80b40d722bd9203cd6e4bc439eb230274393
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-29 01:02:39 -0500

    completed part 4. challenge 27

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -12,6 +12,28 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        
+        boolean found = false;
+        
+        
 
+        try ( Scanner readingInput = new Scanner(Paths.get(file))) {
+            while (readingInput.hasNextLine()) {
+                String row = readingInput.nextLine();
+
+                if (searchedFor.equals(row)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) { //if the boolean is true it prints Found! That's why it is set to false
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found");
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + "failed");
+        }
     }
 }
