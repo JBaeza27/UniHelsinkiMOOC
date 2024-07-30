991bcdb6c92c58422d3d89bba59dc29a0a26a6d8
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-07-29 19:53:50 -0500

    completed part 4. challenge 28

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -6,14 +6,31 @@ public class NumbersFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = 0;
+        
+        
         System.out.print("File? ");
         String file = scanner.nextLine();
         System.out.print("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
-
+        
+        try(Scanner readingFile = new Scanner(Paths.get(file))){
+            while(readingFile.hasNextLine()){
+                int number = Integer.valueOf(readingFile.nextLine()); /*reading next value in file as a integer
+           fileReader.nextLine(); return type is originally a string. we swith it to integer to read the it as an integer.
+                We can check the type of the mehod .nextLine() by shift clicking it.
+*/
+              
+                if(number <= upperBound && number >= lowerBound){
+                    count++;
+                }
+            } 
+        }catch(Exception e){
+            System.out.println("Error reading " + e);
+        }
+        System.out.println("Numbers: " + count);
     }
 
 }
