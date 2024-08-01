155d951a6f6454e0fd745f327d70312fc18cdcd9
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-08-01 00:01:15 -0500

    completed part 4. challenge 29

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,29 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Name of file?");
+        String fileName = scanner.nextLine();
+        
+        try(Scanner readingFile = new Scanner(Paths.get(fileName))){
+            
+            while(readingFile.hasNextLine()){
+                
+                String line = readingFile.nextLine(); /*Rememeber we use readingFile here |not scanner| because then we 
+                                                        wouldn't be directed to the path of the fileName!*/
+                String[] parts = line.split(",");
+                
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
 
+                if (age == 1) {
+                    System.out.println(name + ", age: " + age + " year");
+                } else {
+                    System.out.println(name + ", age: " + age + " years");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
     }
 }
