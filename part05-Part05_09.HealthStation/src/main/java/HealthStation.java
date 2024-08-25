574cd194f6c18a2298d301b774ad6922241b2fbf
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-08-25 14:34:33 -0500

    completed part 5. challenge 9.

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,22 @@
 
 public class HealthStation {
-
-
+    private int weighings;
+                                //changed counter to weighting for readability
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        weighings++;
+        return person.getWeight();
+        
+    }
+    public void feed(Person person){
+        int initialWeight = person.getWeight();
+        person.setWeight(initialWeight + 1);   /*instead of person.setWeight(person.getWeight + 1);
+                                                we will just person.getWeight() other times, so this would affect
+                                                other Person objects.
+                                                                 */
+    }
+    public int weighings(){
+        return weighings;
     }
 
 }
