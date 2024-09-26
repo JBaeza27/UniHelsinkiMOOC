a9eb709ae142c932f9da020b61204426184372c6
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-09-26 14:54:02 -0500

    completed part 5. challenge 11.

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,28 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    public boolean largerThan(Apartment compared){
+        //Checks to see which what is larger in squares
+        if(this.squares > compared.squares){
+            return true;
+        }
+        return false;
+    }
+    
+    public int priceDifference(Apartment compared){
+        int apartment1 = this.princePerSquare * this.squares;
+        int apartment2 = compared.princePerSquare * compared.squares;
+       // Math.abs returns the absolute value. No need to make the logic for it.
+        return Math.abs(apartment1 - apartment2);
+    }
+    public boolean moreExpensiveThan(Apartment compared){
+        //Compares the apartments to see which one is more expensive
+        int apartment1 = this.princePerSquare * this.squares;
+        int apartment2 = compared.princePerSquare * compared.squares;
+        if(apartment1 > apartment2){
+            return true;
+        }
+        return false;
+    }
 
 }
