05129ff26f4da126c2fed7b281fefd6161b3c93c
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-09-26 16:08:28 -0500

    completed part 5. challenge 12.

--- part05-Part05_12.Song/src/main/java/Song.java
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -10,11 +10,36 @@ public class Song {
         this.name = name;
         this.durationInSeconds = durationInSeconds;
     }
+    public boolean equals(Object compared) {
+        // if the variables are located in the same position, they are equal
+        if (this == compared) {
+            return true;
+        }
+
+        // if the compared object is not of type Song, the objects are not equal
+        if (!(compared instanceof Song)) {
+            return false;
+        }
+
+        // converting the object into a Song object
+        Song comparedSong = (Song) compared;
+
+        // if the values of the object variables are equal, the objects are equal
+        // Remeber using .equals is for strings. == is for values.
+        if (this.artist.equals(comparedSong.artist) &&
+            this.name.equals(comparedSong.name) &&
+            this.durationInSeconds == comparedSong.durationInSeconds) {
+            return true;
+        }
+
+ 
+        return false;
+    }
 
     @Override
     public String toString() {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
     }
 
-
+    
 }
