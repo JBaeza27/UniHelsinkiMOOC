44d3d45d12b02c673d3a3d5db10e54849781ffd9
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-10-17 22:36:31 -0500

    completed part 5. challenge 14.

--- part05-Part05_14.Books/src/main/java/Book.java
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -16,5 +16,23 @@ public class Book {
     public int getPublicationYear() {
         return publicationYear;
     }
-
+    
+    public boolean equals(Object compared){
+        //if they are located in same position, they are equal
+        if(this == compared){
+            return true;
+        }
+        //if the compared objects are not of type book
+        if(!(compared instanceof Book)){
+            return false;
+        }
+        //Changing object to Book type
+        Book comparedBook = (Book) compared;
+        
+        if(this.name.equals(comparedBook.name) && this.publicationYear == comparedBook.publicationYear){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
