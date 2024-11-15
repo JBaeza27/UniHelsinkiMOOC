c9b2d9264a5c64b7ca73e1b7f8eb73109cc7cdf9
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2024-11-15 15:35:25 -0600

    added part 6 challenges. completed part 6. challenge 1.

---
+++ part06-Part06_01.Menu/src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // When you have completed the method addMeal(String meal)
+//        // You can delete the comments below
+//        Menu menu = new Menu();
+//        menu.addMeal("Tofu ratatouille");
+//        menu.addMeal("Chilli Coconut Chicken");
+//        menu.addMeal("Chilli Coconut Chicken");
+//        menu.addMeal("Meatballs with mustard sauce");
+//
+//        menu.printMeals();
+
+        // When you have completed the method printMeals()
+        // you can remove the comment below
+//        exactum.printMeals();
+
+        // When you have completed the method clearMenu()
+        // you can remove the comments below
+//        exactum.clearMenu();
+//        exactum.printMeals();
+    }
+}
