8133a769407891cff91b7154d46becb673ff5af3
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-03-27 21:18:19 -0500

    completed part 6. challenge 11.

--- part06-Part06_11.Averages/src/main/java/GradeRegister.java
+++ part06-Part06_11.Averages/src/main/java/GradeRegister.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 public class GradeRegister {
 
     private ArrayList<Integer> grades;
+    private ArrayList<Integer> points;
 
     public GradeRegister() {
         this.grades = new ArrayList<>();
+        this.points = new ArrayList<>();
     }
 
     public void addGradeBasedOnPoints(int points) {
         this.grades.add(pointsToGrade(points));
+        this.points.add(points);
     }
 
     public int numberOfGrades(int grade) {
@@ -22,6 +25,35 @@ public class GradeRegister {
         }
 
         return count;
+    }
+
+    public double averageOfGrades() {
+        int total = 0;
+        int length = this.grades.size();
+
+        if (this.grades.isEmpty()) {
+            return -1;
+        } else {
+            for (int i : grades) {
+                total += i;
+            }
+        }
+        return 1.0 * total / length;
+    }
+
+    public double averageOfPoints() {
+        int total = 0;
+        int length = this.points.size();
+
+        if (this.points.isEmpty()) {
+            return -1;
+        } else {
+            for (int i : points) {
+                total += i;
+            }
+        }
+        return 1.0 * total / length;
+
     }
 
     public static int pointsToGrade(int points) {
@@ -43,4 +75,5 @@ public class GradeRegister {
 
         return grade;
     }
+
 }
