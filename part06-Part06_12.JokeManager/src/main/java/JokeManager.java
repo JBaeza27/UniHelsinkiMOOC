f0e475167e80130518a42047f94df70e94d54110
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-04-15 00:55:01 -0500

    completed part 6. challenge 12.

---
+++ part06-Part06_12.JokeManager/src/main/java/JokeManager.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sierra02
+ */
+import java.util.ArrayList;
+import java.util.Random;
+
+public class JokeManager {
+
+    private ArrayList<String> jokeList;
+
+    public JokeManager() {
+        this.jokeList = new ArrayList<>();
+    }
+
+    public void addJoke(String joke) {
+        this.jokeList.add(joke);
+    }
+
+    public String drawJoke() {
+        if (this.jokeList.isEmpty()) {
+            return "Jokes are in short supply.";
+        } else {
+            Random draw = new Random();
+            int index = draw.nextInt(jokeList.size());
+            return jokeList.get(index);
+        }
+    }
+
+    public void printJokes() {
+        for (String joke : this.jokeList) {
+            System.out.println(joke);
+        }
+    }
+}
---
+++ part06-Part06_12.JokeManager/src/main/java/UserInterface.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sierra02
+ */
+import java.util.Scanner;
+
+public class UserInterface {
+    private JokeManager manager;
+    private Scanner scanner;
+    
+    public UserInterface(JokeManager manager,Scanner scanner){
+        this.manager = manager;
+        this.scanner = scanner;
+    }
+    public void start(){
+        
+    
+        System.out.println("Commands");
+        System.out.println("1 - add a joke ");
+        System.out.println("2 - draw a joke");
+        System.out.println("3 - list jokes");
+        System.out.println("X - stop");
+        String option = scanner.nextLine();
+        
+        switch (option){
+            case "1":
+                System.out.println("Write the joke to be added");
+                String joke = scanner.nextLine();
+                manager.addJoke(joke);
+                break;
+            case "2":
+                String drawJoke = manager.drawJoke();
+                System.out.println(drawJoke);
+                break;
+            case "3":
+                System.out.println("Printing Jokes:");
+                manager.printJokes();
+                break;
+            case "X":
+                return;
+        }
+        
+    }
+}
