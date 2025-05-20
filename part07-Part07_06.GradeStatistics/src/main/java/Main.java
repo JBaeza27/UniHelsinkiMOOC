14072757c7cf5b65804cee877539ecb96223fab0
JBaeza27 <107650433+JBaeza27@users.noreply.github.com>
2025-05-20 18:07:28 -0500

    completed part 7. challenge 6.

--- part07-Part07_06.GradeStatistics/src/main/java/Main.java
+++ part07-Part07_06.GradeStatistics/src/main/java/Main.java
@@ -5,8 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         // Write your program here -- consider breaking the program into 
         // multiple classes.
+        Grades distribution = new Grades();
+
+        System.out.println("Enter point totals, -1 stops: ");
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == -1) {
+                break;
+            }
+
+            distribution.addPointTotal(number);
+        }
+
+        distribution.print();
     }
 }
+
